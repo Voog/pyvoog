f18a42693dfad4bbb0b7e1b4b35a4f9fd56cdb18
Tambet Arak <tambet.arak@gmail.com>
2024-07-16 21:00:30 +0300

    Add the Args class for argument parsing

---
+++ pyvoog/args.py
@@ -0,0 +1,103 @@
+
+""" Sundry command-line argument parsing routines. Provides a set of
+common recognized arguments and allows passing through arguments to
+Gunicorn verbatim, separated from application args by `--`.
+"""
+
+import argparse
+import sys
+
+class Args:
+    def __init__(self, defaults={}, parser_args={}):
+        self._defaults = defaults
+        self._parser_args = parser_args
+        self._parser = None
+        self._parsed_args = None
+        (self._our_argv, self._rest_of_argv) = self._split_command_line()
+
+    @property
+    def parser(self):
+        if not self._parser:
+            defaults = dict(
+                epilog="Any arguments specified after `--` are passed on to the gunicorn server."
+            )
+
+            self._parser = argparse.ArgumentParser(**(defaults | self._parser_args))
+
+        return self._parser
+
+    def add_arguments(self):
+
+        """ Add default arguments to the parser. Override in a subclass to add
+        application-specific arguments.
+        """
+
+        parser = self.parser
+        defaults = self._defaults
+
+        parser.add_argument(
+            "-b", "--bind", default="127.0.0.1", type=str,
+            help="The interface to bind to, 127.0.0.1 by default"
+        )
+        parser.add_argument(
+            "-p", "--port", default=defaults["port"], type=int,
+            help="The port to listen on, {} by default".format(defaults["port"])
+        )
+        parser.add_argument(
+            "-d", "--database", default=defaults["database"], type=str,
+            help="The database URL, {} by default".format(defaults["database"])
+        )
+        parser.add_argument(
+            "-l", "--loglevel", default=defaults["loglevel"], type=str,
+            help="Log level, {} by default".format(defaults["loglevel"])
+        )
+        parser.add_argument(
+            "--extra-loglevel", type=str, help="Log level for extra SQLAlchemy loggers"
+        )
+        parser.add_argument(
+            "--hide-sql-params", action="store_true", help="Hide SQL parameters from log entries"
+        )
+
+    @property
+    def parsed_args(self):
+        if not self._parsed_args:
+            self.add_arguments()
+            self._parsed_args = self.parser.parse_args(self._our_argv)
+
+        return self._parsed_args
+
+    @property
+    def gunicorn_argv(self):
+
+        """ Return the portion of argv separated by `--`. Translate our
+        arguments to gunicorn equivalents and append to the former. Note that
+        this will parse arguments, if this has not happened alredy.
+        """
+
+        args = self.parsed_args
+        gunicorn_argv = self._rest_of_argv.copy()
+
+        if args.port:
+            gunicorn_argv += ["--bind", f"{args.bind}:{args.port}"]
+
+        return gunicorn_argv
+
+    @staticmethod
+    def _split_command_line():
+
+        """ Split the command line on `--` and return the lists of
+        arguments.
+        """
+
+        args = sys.argv[1:]
+        separator_idx = None
+
+        for i, arg in enumerate(args):
+            if arg == "--":
+                separator_idx = i
+                break
+
+        if separator_idx is None:
+            return (args, [])
+
+        return (args[:separator_idx], args[separator_idx + 1:])
