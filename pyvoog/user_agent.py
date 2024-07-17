e0dbe9ddf0a5d0fc4d4d76878957038ec755a08a
Tambet Arak <tambet.arak@gmail.com>
2024-07-17 17:28:49 +0300

    Add the `user_agent` module

---
+++ pyvoog/user_agent.py
@@ -0,0 +1,24 @@
+import requests
+
+class UserAgent:
+
+    """ A thin wrapper around Requests automatically adding a set of headers
+    to the request. A User-Agent may be added as a shortcut.
+    """
+
+    def __init__(self, headers={}, user_agent=None):
+        if user_agent:
+            headers = headers | {"User-Agent": user_agent}
+
+        self.headers = headers
+
+    def __getattr__(self, name):
+        def make_request(url, *args, headers={}, **kwargs):
+            method = getattr(requests, name)
+            headers = (headers or {}) | self.headers
+
+            return method(
+                url, *args, headers=headers, **kwargs
+            )
+
+        return make_request
