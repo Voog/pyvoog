a54abee4e499355cefb8a86b997aa8b0efd977c7
Tambet Arak <tambet.arak@gmail.com>
2024-08-16 17:50:51 +0300

    Amend ControllerTestCase w/ `get_response`

--- pyvoog/testing/controller_test_case.py
+++ pyvoog/testing/controller_test_case.py
@@ -4,14 +4,46 @@ from unittest import TestCase
 
 from pyvoog.db import get_plain_session
 from pyvoog.testing.util.controllers import controller_fixture
+from pyvoog.testing.util.models import temporary_object
+from pyvoog.util import mapping_to_namedtuple
 
 class ControllerTestCase(TestCase):
+
+    """ A controller test suite base class, providing convenience
+    routines for making typical requests. Respects the following
+    attributes on the object:
+
+    - ENDPOINT - the default endpoint for making requests.
+    - jwt_secret - JWT secret for constructing the access token.
+    - jwt_payload - JWT payload for constructing the access token.
+    """
+
+    @contextmanager
+    def get_response(self, model, model_args, endpoint=None):
+
+        """ A context manager constructing a temporary instance of `model`
+        with `model_args`, making a GET request to an endpoint and yielding
+        the response.
+
+        `endpoint` may be a string or a callable (in which case the callable is
+        invoked with `obj` and its result used as the effective endpoint).
+        `self.ENDPOINT` is used as the default callback.
+        """
+
+        with self._bound_controller_fixture as ua:
+            with temporary_object(model, **model_args) as obj:
+                if callable(endpoint):
+                    effective_endpoint = endpoint(obj)
+                else:
+                    effective_endpoint = endpoint or self.ENDPOINT
+
+                yield ua.get(effective_endpoint)
+
     @contextmanager
     def post_response(self, model=None, payload=None):
 
-        """ A context manager making a POST request to `self.ENDPOINT` with the
-        given payload, using `self.jwt_secret` and `self.jwt_payload` for
-        constructing the authentication token, and yielding the response.
+        """ A context manager making a POST request to `self.ENDPOINT` and
+        yielding the response.
 
         If the request is successful, `model` is present and the response body
         is a JSON object containing `id`, the persisted object is cleaned up.
