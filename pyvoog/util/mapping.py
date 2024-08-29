5e08dcaef4982fbfbb791f3391333c017f24805e
Tambet Arak <tambet.arak@gmail.com>
2024-08-29 17:24:04 +0300

    Add the `filter_mapping` mapping utility

--- pyvoog/util/mapping.py
+++ pyvoog/util/mapping.py
@@ -22,3 +22,19 @@ def mapping_to_namedtuple(mapping, class_name="MappingToNamedtuple"):
         nt = nt._replace(**{k: mapping_to_namedtuple(v, class_name)})
 
     return nt
+
+def filter_mapping(fn, mapping):
+
+    """ A `filter` counterpart for mapping types, with the same signature
+    and parameter semantics (as far as applicable to mappings). Filtering
+    applies to values.
+
+    See:
+
+    https://docs.python.org/3/library/functions.html#filter
+    """
+
+    if fn is None:
+        fn = lambda x: x
+
+    return {k: v for k, v in mapping.items() if fn(v)}
