4de120397d7be27af3f9d406ef0d605d9a5887b3
Tambet Arak <tambet.arak@gmail.com>
2024-09-06 11:47:39 +0300

    Add the `*_suffix` attributes to Resource
    Add flexibility to customizing controller module and class names. Make
    `x_controller` the default module name when resource's name is `x`.

--- pyvoog/router/resource.py
+++ pyvoog/router/resource.py
@@ -11,16 +11,22 @@ class Resource:
       class name and the path prefix for default endpoints.
     - ctrlr_class_name - Explicitly set the name of controller class mapping
       to the Resource.
+    - ctrlr_class_suffix - Set the controller class name suffix when
+      inferring the class name from `name`, "Controller" by default.
     - ctrlr_module_name - Explicitly set the suffix of the controller module
       mapping to the Resource. The router's `controller_ns` and any
       encapsulating namespace's path prefix are nevertheless respected.
+    - ctrlr_module_suffix - Set the module name suffix when inferring the
+      module name from `name`, "_controller" by default.
     """
 
     name: str = None
     endpoints: list = []
     include_default_endpoints: bool = False
     ctrlr_class_name: str = None
+    ctrlr_class_suffix: str = "Controller"
     ctrlr_module_name: str = None
+    ctrlr_module_suffix: str = "_controller"
 
     def __attrs_post_init__(self):
         if not self.name and (
@@ -34,7 +40,7 @@ class Resource:
             )
 
         if not self.ctrlr_class_name:
-            self.ctrlr_class_name = f"{pascalcase(self.name)}Controller"
+            self.ctrlr_class_name = f"{pascalcase(self.name)}{self.ctrlr_class_suffix}"
 
         if not self.ctrlr_module_name:
-            self.ctrlr_module_name = self.name
+            self.ctrlr_module_name = f"{self.name}{self.ctrlr_module_suffix}"
