Voog/pyvoog

commit 1de95edb5c5a5e404b2b7986ee077d65f0eb9412

Guard against MultilineFormatter failures
When breaking MultiLineLogRecords on newlines, message placeholders may
not be present in the resulting constituent records, resulting in
formatting failures.  Fall back to formatting the entire record in such
cases.

pyvoog/logging.py
@@ -17,7 +17,7 @@ class PrefixedLogRecord(logging.LogRecord):
         super().__init__(name, *args, **kwargs)
         self.prefix = "" if name == "root" else "[{}] ".format(name)
 
-class MultilineLogRecord(logging.LogRecord):
+class MultilineLogRecord(PrefixedLogRecord):
 
     """ A LogRecord subclass splitting incoming messages on newlines and
     converting each to a LogRecord of type `constituent_cls`
@@ -29,7 +29,6 @@ class MultilineLogRecord(logging.LogRecord):
         )
 
         super().__init__(name, level, fn, lno, msg, *args, **kwargs)
-
 
 class MultilineFormatter(logging.Formatter):
 
@@ -44,10 +43,18 @@ class MultilineFormatter(logging.Formatter):
                 f"but received {type(record).__name__}"
             )
 
-        return "\n".join(
-            super(MultilineFormatter, self).format(line_record)
-            for line_record in record.constituents
-        )
+        for line_record in record.constituents:
+            super_instance = super(MultilineFormatter, self)
+
+            try:
+                return super_instance.format(line_record)
+            except TypeError:
+
+                # When breaking on newlines, message placeholders may not be present in
+                # the resulting constituent records, resulting in formatting failures.
+                # Fall back to formatting the entire record in such cases.
+
+                super_instance.format(record)
 
 class ContextfulLogger:
 

commit 66ac179b657f29d663dc28f5305890209bb4b291

Fix MultilineFormatter single line bug
Fix a bug introduced in 1de95edb5, resulting in logging only the first
line of a MultilineLogRecord. The loop over constituents should not
return, but collect all formatted lines into a list for final
concatenation at the end of the method.

pyvoog/logging.py
@@ -37,6 +37,8 @@ class MultilineFormatter(logging.Formatter):
     """
 
     def format(self, record):
+        formatted_lines = []
+
         if not isinstance(record, MultilineLogRecord):
             raise TypeError(
                 "MultilineFormatter can only format MultilineLogRecords, "
@@ -47,14 +49,17 @@ class MultilineFormatter(logging.Formatter):
             super_instance = super(MultilineFormatter, self)
 
             try:
-                return super_instance.format(line_record)
+                formatted_lines.append(super_instance.format(line_record))
             except TypeError:
 
                 # When breaking on newlines, message placeholders may not be present in
                 # the resulting constituent records, resulting in formatting failures.
                 # Fall back to formatting the entire record in such cases.
 
-                super_instance.format(record)
+                formatted_lines = (super_instance.format(record),)
+                break
+
+        return "\n".join(formatted_lines)
 
 class ContextfulLogger:
 