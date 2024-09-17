0f1f3de61f4f7593c80a5e92227190336c3b0d03
Tambet Arak <tambet.arak@gmail.com>
2024-09-17 12:03:43 +0300

    Add the `jwt` attribute to UserAgent

--- pyvoog/user_agent.py
+++ pyvoog/user_agent.py
@@ -5,19 +5,23 @@ from attrs import define, field
 @define
 class UserAgent:
 
-    """ A thin wrapper around Requests automatically adding a set of headers
-    to the request. A User-Agent may be added as a shortcut. All other
-    arguments to `request` (or rather its wrappers) may be set on the
-    instance by `default_rq_args`.
+    """ A thin wrapper around Requests automatically adding a set of
+    headers to the request. A User-Agent and a JWT token passed on the
+    Authorization header may be added as shortcuts. All other arguments to
+    `request` (or rather its wrappers) may be set on the instance by
+    `default_rq_args`.
     """
 
-    user_agent: str = None
-    headers: dict = {}
     default_rq_args: dict = {}
+    headers: dict = {}
+    jwt: str = None
+    user_agent: str = None
 
     def __attrs_post_init__(self):
         if self.user_agent:
             self.headers = self.headers | {"User-Agent": self.user_agent}
+        if self.jwt:
+            self.headers = self.headers | {"Authorization": f"Bearer {self.jwt}"}
 
     def __getattr__(self, name):
         def make_request(*args, headers={}, **kwargs):
